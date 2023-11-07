01a38ea770aa4f3e7b485d6a8325e04857cfb6d7
barhmate <barhmate.mohamed@gmail.com>
2023-11-07 14:20:08 -0500

    create userCrud

---
+++ functions/userCrud.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * Create user 
+ * 
+ */
+function createUser(array $data)
+{
+    global $conn;
+    
+    $query = "INSERT INTO user VALUES (NULL, ?, ?, ?)";
+
+    if ($stmt = mysqli_prepare($conn, $query)) {
+        
+        mysqli_stmt_bind_param(
+            $stmt,
+            "sss",
+            $data['user_name'],
+            $data['email'],
+            $data['pwd']
+        );
+
+        /* Exécution de la requête */
+        $result = mysqli_stmt_execute($stmt);
+    }
+}
+/**
+ * Get all users
+ */
+function getAllUsers()
+{
+    global $conn;
+    $result = mysqli_query($conn, "SELECT * FROM user");
+
+    $data = [];
+    $i = 0;
+    while ($rangeeData = mysqli_fetch_assoc($result)) {
+        $data[$i] = $rangeeData;
+        $i++;
+    };
+
+    return $data;
+}
+/**
+ * Get user by id
+ */
+
+ //Todo: edit to prepare
+function getUserById(int $id)
+{
+    global $conn;
+    $result = mysqli_query($conn, "SELECT * FROM user WHERE id = " . $id);
+
+    $data = mysqli_fetch_assoc($result);
+
+    return $data;
+}
+/**
+ * Update user
+ */
+function updateUser(array $data)
+{
+    global $conn;
+
+    $query = "UPDATE user SET user_name = ?, email = ?, pwd = ?
+            WHERE user.id = ?;";
+
+    if ($stmt = mysqli_prepare($conn, $query)) {
+
+        mysqli_stmt_bind_param(
+            $stmt,
+            "sssi",
+            $data['user_name'],
+            $data['email'],
+            $data['pwd'],
+            $data['id'],
+        );
+
+        /* Exécution de la requête */
+        $result = mysqli_stmt_execute($stmt);
+    }
+}
+/**
+ * Delete user
+ */
+function deleteUser(int $id)
+{
+    global $conn;
+
+    $query = "DELETE FROM user
+                WHERE user.id = ?;";
+
+    if ($stmt = mysqli_prepare($conn, $query)) {
+
+        mysqli_stmt_bind_param(
+            $stmt,
+            "i",
+            $id,
+        );
+
+        /* Exécution de la requête */
+        $result = mysqli_stmt_execute($stmt);
+    }
+}
